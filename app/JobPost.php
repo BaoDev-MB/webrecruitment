27e423ef1059a52058990e2e334b2ee5baa78ea7
quyen <lelamphuongquyen@gmail.com>
2020-06-26 14:54:24 +0700

    create migration

--- app/JobPost.php
+++ app/JobPost.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class JobPost extends Model
 {
     //
-    protected $table = 'job_posts';
+    protected $table = 'jobposts';
 }
---
+++ database/migrations/2020_06_26_054324_create_job_posts_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('jobposts', function (Blueprint $table) {
+        $table->increments('id')->length(11);
+        $table->unsignedInteger('majors');
+        $table->foreign('majors')->references('id')->on('majors');
+        $table->unsignedInteger('companies');
+        $table->foreign('companies')->references('id')->on('companies');
+//        $table->foreignId('majors')->constrained('majors'); //
+//        $table->foreignId('companies')->constrained('companies');
+        $table->string('name')->nullable();
+        $table->date('date_posted')->nullable();
+        $table->date('date_expires')->nullable();
+        $table->string('key_word')->nullable();
+        $table->decimal('salary_upto')->nullable();
+        $table->string('url')->nullable();
+        $table->string('description')->nullable();
+        $table->string('location')->nullable();
+        $table->string('job_type')->nullable();
+        $table->integer('active');
+
+        $table->timestamps();
+    });
+//        Schema::table('jobposts', function($table)
+//        {
+//            $table->foreign('majors')->references('id')->on('majors');
+//            $table->foreign('companies')->references('id')->on('companies');
+//        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobposts');
+    }
+}
