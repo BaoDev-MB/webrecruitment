0ce6d8126a2c2684025195adacf88d46f1153844
17130010@st.hcmuaf.edu.vn <Baonguyen1999>
2020-07-13 14:53:21 +0700

    fix logic in ui login register

--- resources/views/pages/auth.blade.php
+++ resources/views/pages/auth.blade.php
@@ -3,8 +3,12 @@
 @section('title', "Login - Register")
 
 @section('content')
+<!-- Titlebar
+    ================================================== -->
+
 <div id="titlebar" class="single">
     <div class="container">
+
         <div class="sixteen columns">
             <h2>My Account</h2>
             <nav id="breadcrumbs">
@@ -15,24 +19,28 @@
                 </ul>
             </nav>
         </div>
+
     </div>
 </div>
 
+
 <!-- Content
-================================================== -->
+    ================================================== -->
 
 <!-- Container -->
 <div class="container">
+
     <div class="my-account">
+
         <ul class="tabs-nav">
-            <li class=""><a href="#tab1">Login</a></li>
+            <li class=""><a href="#tab1">Login</a> </li>
             <li class=""><a href="#tab2">Register</a></li>
         </ul>
 
         <div class="tabs-container">
             <!-- Login -->
             <div class="tab-content" id="tab1" style="display: none;">
-                <form method="post" class="login">
+                <form method="POST" action="{{url('login')}}" class="login">
                     @csrf
                     @error('mes')
                     <small class="form-text text-danger">{{ $message }}</small>
@@ -41,7 +49,7 @@
                     @if(Session::has('ok'))
                     <small class="form-text text-success">{{ Session::get('ok') }}</small>
                     @endif
-                    <p class="form">
+                    <p class="form-row form-row-wide">
                         <label for="email">Email:
                             <i class="ln ln-icon-Email"></i>
                             <input type="text" class="input-text" name="email" id="email" value="{{ old('email') }}" />
@@ -51,7 +59,7 @@
                         @enderror
                     </p>
 
-                    <p class="form">
+                    <p class="form-row form-row-wide">
                         <label for="pass">Password:
                             <i class="ln ln-icon-Lock-2"></i>
                             <input class="input-text" type="password" name="pass" id="pass" } />
@@ -61,25 +69,27 @@
                         @enderror
                     </p>
 
-                    <p class="form-group">
-                        <input type="submit" class="btn-block border fw mt-3" name="login" value="Login" />
+                    <p class=" form-row">
+                        <input type="submit" class="button border fw margin-top-10" name="login" value="Login" />
 
                         <label for="rememberme" class="rememberme">
-                            <input name="rememberme" type="checkbox" id="rememberme" value="forever" />
-                            Remember Me</label>
+                            <input name="rememberme" type="checkbox" id="rememberme" value="forever" /> Remember
+                            Me</label>
                     </p>
 
                     <p class="lost_password">
-                        <a href="{{route('forgetpass')}}">Lost Your Password?</a>
+                        <a href="{{route('forgetpassword')}}">Lost Your Password?</a>
                     </p>
+
                 </form>
             </div>
 
             <!-- Register -->
             <div class="tab-content" id="tab2" style="display: none;">
-                <form method="post" class="register">
+
+                <form method="post" action="{{ url('register') }}" class="register">
                     @csrf
-                    <p class="form">
+                    <p class="form-row form-row-wide">
                         <label for="r_firstname">First Name:
                             <i class="ln ln-icon-Male"></i>
                             <input type="text" class="input-text" name="r_firstname" id="r_firstname"
@@ -89,7 +99,7 @@
                         <small class="form-text text-danger">{{ $message }}</small>
                         @enderror
                     </p>
-                    <p class="form">
+                    <p class="form-row form-row-wide">
                         <label for="r_lastname">Last Name:
                             <i class="ln ln-icon-Male"></i>
                             <input type="text" class="input-text" name="r_lastname" id="r_lastname"
@@ -100,7 +110,7 @@
                         @enderror
                     </p>
 
-                    <p class="form">
+                    <p class="form-row form-row-wide">
                         <label for="r_email">Email Address:
                             <i class="ln ln-icon-Mail"></i>
                             <input type="text" class="input-text" name="r_email" id="r_email"
@@ -111,7 +121,7 @@
                         @enderror
                     </p>
 
-                    <p class="form">
+                    <p class="form-row form-row-wide">
                         <label for="r_pass">Password:
                             <i class="ln ln-icon-Lock-2"></i>
                             <input class="input-text" type="password" name="r_pass" id="r_pass" />
@@ -121,7 +131,7 @@
                         @enderror
                     </p>
 
-                    <p class="form">
+                    <p class="form-row form-row-wide">
                         <label for="r_repass">Repeat Password:
                             <i class="ln ln-icon-Lock-2"></i>
                             <input class="input-text" type="password" name="r_repass" id="r_repass" />
@@ -131,14 +141,18 @@
                         @enderror
                     </p>
 
-                    <p class="form-group">
-                        <input type="submit" class="btn-block border fw mt-3" name="register" value="Register" />
+                    <p class="form-row">
+                        <input type="submit" class="button border fw margin-top-10" name="register" value="Register" />
                     </p>
+
                 </form>
             </div>
         </div>
     </div>
 </div>
+
+
+
 
 
 @endsection()
